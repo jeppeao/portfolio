6a63caddffb3431e726dca17b0f8d5ecf1cb483a
jeppeao <jeppeao@gmail.com>
2023-06-17 11:04:26 +0200

    added ProjectView component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Intro from './components/Intro/Intro'
 import Menu from './components/Menu/Menu'
 import Footer from './components/Footer/Footer';
+import ProjectView from './components/ProjectView/ProjectView';
 
 function App() {
   const [isDarkmode, setIsDarkmode] = useState(true);
@@ -9,6 +10,12 @@ function App() {
     <>
       <Menu isDarkmode={isDarkmode} setDarkmode={setIsDarkmode}/>
       <Intro isDarkmode={isDarkmode}/>
+      <ProjectView 
+        isDarkmode={isDarkmode}
+        title={"Song Recommendations"}
+        imgSrc={"/Top-recommendations.png"}
+        description={"Find music based on spotify liked songs. This app will fetch recommendations for each of a user's liked songs on spotify and rank them on how many times they were recommended. Maybe the songs recommended over and over again are a match? Made using Next.js, tailwind and the spotify API."}
+      />
       <Footer isDarkmode={isDarkmode}/>
     </>
   )
--- src/components/ProjectView/ProjectView.tsx
+++ src/components/ProjectView/ProjectView.tsx
@@ -1,6 +1,42 @@
-const ProjectView = () => {
+import styles from "./ProectView.module.css"
+
+interface ProjectViewProps {
+  isDarkmode: boolean;
+  title: string;
+  imgSrc: string;
+  description: string;
+}
+
+const ProjectView = ({ 
+  isDarkmode,
+  title,
+  imgSrc,
+  description
+} : ProjectViewProps) => {
   return (
-    <div>Project</div>
+    <div 
+      className={`
+        ${styles.container}
+        ${isDarkmode ? "darkmode" : "lightmode"}
+    `}>
+      <div 
+        className={`
+          ${styles.content}
+      `}>
+        <h3>{title}</h3>
+        <img 
+          src={imgSrc} 
+          className={`${styles.image}`}
+          width="100%"
+        />
+        <p 
+          className={`
+          ${styles.description}
+        `}>
+          {description}
+        </p>
+      </div>
+    </div>
   )
 }
 
